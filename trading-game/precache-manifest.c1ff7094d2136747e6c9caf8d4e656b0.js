dd1a62ae883cb40d79b60a1b479df9c302fdf67f
fpassow <fpassow@yahoo.com>
2022-01-16 12:09:42 -0500

    Edits. And moved trading-game web content into this project.

---
+++ trading-game/precache-manifest.c1ff7094d2136747e6c9caf8d4e656b0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "a281cc63d9459b08be14",
+    "url": "/trading-game/static/js/runtime~main.68757109.js"
+  },
+  {
+    "revision": "96fc9cff710dd7803457",
+    "url": "/trading-game/static/js/main.e3ac001c.chunk.js"
+  },
+  {
+    "revision": "611254752b169ab4b11a",
+    "url": "/trading-game/static/js/2.7e0ba1e2.chunk.js"
+  },
+  {
+    "revision": "2eae3f2702cb049d359f824d77a7e965",
+    "url": "/trading-game/index.html"
+  }
+];
